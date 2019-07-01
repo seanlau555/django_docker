33871fcf0c145ccdafd7a647327c7a9281564fe7
劉子漢 (軟件工程開發部)Sean Lau <seanlau@hk01.com>
2019-07-01 18:36:45 +0800

    first commit

---
+++ app/on-container-start.sh
@@ -0,0 +1,14 @@
+
+# Create migrations based on django models
+python manage.py makemigrations
+
+# Migrate created migrations to database
+python manage.py migrate
+
+# Copy all static files to specific directory for nginx
+python manage.py collectstatic
+
+# Start gunicorn server at port 8000 and keep an eye for app code changes
+# If changes occur, kill worker and start a new one
+# gunicorn --reload myproject.wsgi:application -b 0.0.0.0:8000
+gunicorn -c gunicorn.conf myproject.wsgi
