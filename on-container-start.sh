4dd00d9dcae7e343bcc072a03ac85f53d8e30f2e
sean <lthon0225@gmail.com>
2019-08-31 23:16:01 +0800

    fix: combine one dockerfile

--- on-container-start.sh
+++ on-container-start.sh
@@ -1,9 +1,9 @@
 
 # Create migrations based on django models
-python manage.py makemigrations
+python app/manage.py makemigrations
 
 # Migrate created migrations to database
-python manage.py migrate
+python app/manage.py migrate
 
 # Copy all static files to specific directory for nginx
 # python manage.py collectstatic
@@ -11,4 +11,4 @@ python manage.py migrate
 # Start gunicorn server at port 8000 and keep an eye for app code changes
 # If changes occur, kill worker and start a new one
 # gunicorn --reload myproject.wsgi:application -b 0.0.0.0:8000
-gunicorn -c gunicorn.conf myproject.wsgi
+gunicorn -c gunicorn.conf app/myproject.wsgi
